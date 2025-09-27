e77d25ec4bde7f1b718f4cfc693499afb8bb0235
ItchyTrack <benherman345@gmail.com>
2025-09-27 12:28:02 -0400

    work now

--- src/cli/commandManager.h
+++ src/cli/commandManager.h
@@ -2,7 +2,7 @@
 #define commandManager_h
 
 #include "command.h"
-#include "environment/environment.h"
+class Environment;
 
 class CommandManager {
 public:
@@ -11,8 +11,9 @@ public:
 
     void registerCommand(std::unique_ptr<Command>&& command);
     void run(const std::vector<std::string>& commandParams, Environment& environment);
+
 private:
     std::map<std::string, std::unique_ptr<Command>> commandMap;
 };
 
-#endif
+#endif /* commandManager_h */
