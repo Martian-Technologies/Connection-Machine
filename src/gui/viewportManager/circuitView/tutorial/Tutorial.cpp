0e16e94e84910f4cdc5b3f5d9ef5a5216a6a5042
Yuguang <yuguanglee2003@gmail.com>
2025-11-21 21:26:27 -0500

    more tutoral implemenaton

--- src/gui/viewportManager/circuitView/tutorial/Tutorial.cpp
+++ src/gui/viewportManager/circuitView/tutorial/Tutorial.cpp
@@ -20,3 +20,29 @@ void Tutorial::nextState() {
         applyState(states[currentIndex]);
     }
 }
+
+void Tutorial::previousState() {
+    if (currentIndex - 1 >= 0) {
+        currentIndex--;
+        applyState(states[currentIndex]);
+    }
+}
+
+TutorialState* Tutorial::getCurrentState() const {
+    if (currentIndex < 0 || currentIndex >= (int)states.size()) return nullptr;
+    return &states[currentIndex];
+}
+
+
+void Tutorial::applyState(const TutorialState& state) {
+    SharedCircuit circuit = circuitView->getCircuit();
+    if (!circuit) return;
+    circuit->clearCircuit();
+    for (const auto& inst : state.blocks) {
+        circuit->tryInsertBlock(inst.position, inst.orientation, inst.type);
+    }
+    circuitView->getEventRegister().doEvent(
+        Event("status bar changed", state.name)
+    );
+}
+
